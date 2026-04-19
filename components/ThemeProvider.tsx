c1703c37bf6d5c217083c74a2877bafbf13bf22c
martinboris-alt <martinboris@projectmechanicalpro.com>
2026-04-19 14:07:38 +0100

    Fix ThemeProvider API para next-themes 0.4.x

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes";
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
-      enableSystem={false}
+      disableTransitionOnChange={false}
+      {...props}
     >
       {children}
     </NextThemesProvider>
